a374fe90869dd1c7d9ce1d837b65485a18db619f
lucastreille <lucas.treille1.0@gmail.com>
2024-11-26 11:44:01 +0100

    Update  api url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  // baseURL: "http://localhost:5001/api",
+  baseURL: "https://cv-tech-back.onrender.com/",
 });
 
 
